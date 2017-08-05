069ddf1651d2e6b1e7146c59ba39514f5aeee1b3
monirz <monir.smith@gmail.com>
2017-08-05 13:53:46 +0600

    missing method added, route path matching fixed

--- router.go
+++ router.go
@@ -1,6 +1,7 @@
 package track
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 )
@@ -24,6 +25,7 @@ func hello(w http.ResponseWriter, r *http.Request, params map[string]string) {
 
 //New instantiate a new Router
 func New() *Router {
+	fmt.Println("log")
 	return &Router{}
 }
 
@@ -37,41 +39,79 @@ func (r *Router) Post(path string, h Handle) {
 	r.routes = append(r.routes, rt)
 }
 
+//Get handles HTTP POST request through ServeHTTP
+func (r *Router) Get(path string, h Handle) {
+	rt := route{}
+	rt.Path = path
+	rt.Method = "GET"
+	rt.Handler = h
+
+	r.routes = append(r.routes, rt)
+}
+
+//Patch handles HTTP POST request through ServeHTTP
+func (r *Router) Patch(path string, h Handle) {
+	rt := route{}
+	rt.Path = path
+	rt.Method = "PATCH"
+	rt.Handler = h
+
+	r.routes = append(r.routes, rt)
+}
+
+//Delete handles HTTP POST request through ServeHTTP
+func (r *Router) Delete(path string, h Handle) {
+	rt := route{}
+	rt.Path = path
+	rt.Method = "DELETE"
+	rt.Handler = h
+
+	r.routes = append(r.routes, rt)
+}
+
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	reqPath := strings.Split(req.URL.Path, "/")[1:]
+	reqPath := req.URL.Path
+	if strings.HasSuffix(reqPath, "/") {
+		reqPath = strings.TrimSuffix(reqPath, "/")
+	}
+	reqParts := strings.Split(reqPath, "/")[1:]
+
 	params := make(map[string]string)
-	for _, route := range r.routes {
+	for i, route := range r.routes {
 
-		if route.Method != req.Method {
-			http.NotFound(w, req)
-			return
-		}
+		routeParts := strings.Split(route.Path, "/")[1:]
 
-		routePath := strings.Split(route.Path, "/")[1:]
-		if len(routePath) != len(routePath) {
-			http.NotFound(w, req)
-			return
+		if len(routeParts) != len(reqParts) || route.Method != req.Method {
+			if len(r.routes)-1 == i {
+				http.NotFound(w, req)
+				return
+			}
+			continue
 		}
 
 		isParam := false
+		var paramTrimmed string
 
-		for k, v := range routePath {
+		for k, v := range routeParts {
+
 			if strings.HasPrefix(v, ":") {
 				isParam = true
-				paramTrimmed := strings.TrimPrefix(v, ":")
-				params[paramTrimmed] = reqPath[k]
+				paramTrimmed = strings.TrimPrefix(v, ":")
+				params[paramTrimmed] = reqParts[k]
 
 			}
 
 			if !isParam {
-				if v != reqPath[k] {
+				if v != reqParts[k] {
 					http.NotFound(w, req)
 					return
 				}
 			}
 
-			route.Handler(w, req, params)
 		}
+
+		route.Handler(w, req, params)
+		return
 
 	}
 }
